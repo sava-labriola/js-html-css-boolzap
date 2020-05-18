9cd78f78d080a07bfbd30dd1cb59e08b025ad03a
sava-labriola <labriolasava00@gmail.com>
2020-05-18 15:59:30 +0200

    template con Handlebars fatto

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
 //milestone 1
 
-//invio messaggio e risposta dopo un secondo
+//invio messaggio e risposta dopo un secondo (metodo clone)
+
+// $('.microphone').click(function() {
+//     var testo_inserito = $('.write').val();
+//     $('.write').val('');
+//     var nuovo_messaggio = $('.template .message-uscita-wrapper').clone();
+//     $('.chat-wrapper.active').append(nuovo_messaggio);
+//     $('.message-uscita-wrapper:last-of-type p').text(testo_inserito);
+//     setTimeout(function(){
+//         var nuovo_messaggio_arrivo = $('.template .message-arrivo-wrapper').clone();
+//         $('.chat-wrapper.active').append(nuovo_messaggio_arrivo);
+//         $('.message-arrivo-wrapper:last-of-type p').text('ok');
+//     }, 1000);
+// })
+
+//invio messaggio e risposta dopo un secondo (metodo template Handlebars)
 
 $('.microphone').click(function() {
+//recupero struttura template html
+    var template_html = $('#sent').html();
+//funzione per utilizzare il template
+    var template_function = Handlebars.compile(template_html);
+//recupero del messaggio scritto dall' utente
     var testo_inserito = $('.write').val();
     $('.write').val('');
-    var nuovo_messaggio = $('.template .message-uscita-wrapper').clone();
-    $('.chat-wrapper.active').append(nuovo_messaggio);
-    $('.message-uscita-wrapper:last-of-type p').text(testo_inserito);
+//oggetto con il messaggio inserito dall' utente
+    var messaggio_inserito = {
+        'messaggio' : testo_inserito
+    };
+//html finale con Handlebars
+    var html_finale = template_function(messaggio_inserito);
+//append del messaggio nella chat
+    $('.chat-wrapper.active').append(html_finale);
+//ricevo un messaggio di risposta dopo un secondo
     setTimeout(function(){
-        var nuovo_messaggio_arrivo = $('.template .message-arrivo-wrapper').clone();
-        $('.chat-wrapper.active').append(nuovo_messaggio_arrivo);
-        $('.message-arrivo-wrapper:last-of-type p').text('ok');
+//recupero struttura template html
+        var template_html = $('#received').html();
+//funzione per utilizzare il template
+        var template_function = Handlebars.compile(template_html);
+//oggetto con il messaggio inserito dall' utente
+        var messaggio_inserito = {
+            'messaggio' : 'ok'
+        };
+//html finale con Handlebars
+        var html_finale = template_function(messaggio_inserito);
+//append del messaggio nella chat
+        $('.chat-wrapper.active').append(html_finale);
     }, 1000);
 })
 
